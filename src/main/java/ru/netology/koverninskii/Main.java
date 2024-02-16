45a0388b7f18ed62bbadcbbc3579b26899a63285
koves <kovestas1@yandex.ru>
2024-02-16 19:05:17 +0300

    16.02

--- src/main/java/ru/netology/koverninskii/Main.java
+++ src/main/java/ru/netology/koverninskii/Main.java
@@ -2,6 +2,15 @@ package ru.netology.koverninskii;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a = 1003;
+        String b = "Netology number one!";
+        if (a>1000){
+            System.out.println(b);
+        }
+        else {
+            System.out.println("Hello world");
+        }
+
     }
 }
